2d57b8f0ca67f71526f4bb14c2db572343d639e1
Lonus Lan <39768609+lonuslan@users.noreply.github.com>
2020-12-14 16:45:19 +0800

    Create dockerrun.sh

---
+++ dockerrun.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+## 用于https://github.com/mixool/dockershc项目安装运行dockerrun的脚本
+
+if [[ ! -f "/workerone" ]]; then
+    # install and rename
+    wget -qO- https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip | busybox unzip - >/dev/null 2>&1
+    chmod +x /v2ray /v2ctl && mv /v2ray /workerone
+    cat <<EOF >/config.json
+{
+    "inbounds": 
+    [
+        {
+            "port": "3000","listen": "0.0.0.0","protocol": "vless",
+            "settings": {"clients": [{"id": "86892e01-cf95-45e1-82a8-9554cdd3bcc3"}],"decryption": "none"},
+            "streamSettings": {"network": "ws","wsSettings": {"path": "/website"}}
+        }
+    ],
+    "outbounds": 
+    [
+        {"protocol": "freedom","tag": "direct","settings": {}},
+        {"protocol": "blackhole","tag": "blocked","settings": {}}
+    ],
+    "routing": 
+    {
+        "rules": 
+        [
+            {"type": "field","outboundTag": "blocked","ip": ["geoip:private"]},
+            {"type": "field","outboundTag": "block","protocol": ["bittorrent"]},
+            {"type": "field","outboundTag": "blocked","domain": ["geosite:category-ads-all"]}
+        ]
+    }
+}
+EOF
+else
+    # start 
+    /workerone -config /config.json >/dev/null 2>&1
+fi
